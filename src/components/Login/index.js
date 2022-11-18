9aec9cad9168bcc21580896bf0fd4e1bc85fbeab
IcyflameIV <107233500+IcyflameIV@users.noreply.github.com>
2022-11-18 15:23:32 +0545

    first

---
+++ src/components/Login/index.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import Swal from 'sweetalert2';
+
+const Login = ({ setIsAuthenticated }) => {
+  const adminEmail = 'admin@example.com';
+  const adminPassword = 'qwerty';
+
+  const [email, setEmail] = useState('admin@example.com');
+  const [password, setPassword] = useState('qwerty');
+
+  const handleLogin = e => {
+    e.preventDefault();
+
+    if (email === adminEmail && password === adminPassword) {
+      Swal.fire({
+        timer: 1000,
+        showConfirmButton: false,
+        willOpen: () => {
+          Swal.showLoading();
+        },
+        willClose: () => {
+          localStorage.setItem('is_authenticated', true);
+          setIsAuthenticated(true);
+
+          Swal.fire({
+            icon: 'success',
+            title: 'Successfully logged in!',
+            showConfirmButton: false,
+            timer: 1000,
+          });
+        },
+      });
+    } else {
+      Swal.fire({
+        timer: 1500,
+        showConfirmButton: false,
+        willOpen: () => {
+          Swal.showLoading();
+        },
+        willClose: () => {
+          Swal.fire({
+            icon: 'error',
+            title: 'Error!',
+            text: 'Incorrect email or password.',
+            showConfirmButton: true,
+          });
+        },
+      });
+    }
+  };
+
+  return (
+    <div className="small-container">
+      <form onSubmit={handleLogin}>
+      <div className="col-12  p-3"><h1><i class="bi bi-person"></i></h1></div>
+        <h1>Admin Login</h1>
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          type="email"
+          name="email"
+          onChange={e => setEmail(e.target.value)}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          type="password"
+          name="password"
+          onChange={e => setPassword(e.target.value)}
+        />
+        <input style={{ marginTop: '12px' }} type="submit" value="Login" />
+      </form>
+    </div>
+  );
+};
+
+export default Login;
